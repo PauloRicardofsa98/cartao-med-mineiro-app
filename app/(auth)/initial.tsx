18565fe9c8ca2afcbc5d062dcfe857df71f83547
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-20 21:13:04 -0300

    chore: update initial commit

--- app/(auth)/initial.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export const InitialPage = () => {
-  return (
-    <View>
-      <Text>Initial Page</Text>
-    </View>
-  );
-};
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,43 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { Stack } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "react-native-reanimated";
-import { Text } from "react-native";
-import { InitialPage } from "./(auth)/initial";
+import Auth from "./(auth)/login";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    //chama o provide de login
-    <InitialPage />
+    <SafeAreaProvider>
+      <Auth />
+    </SafeAreaProvider>
   );
 }
