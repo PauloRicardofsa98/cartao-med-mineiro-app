8162e2a89ecdf4e45dc78a155c1df7a22337ac0f
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-22 11:39:31 -0300

    feat: add InputIcon component for styled input with icon

---
+++ components/ui/input-icon.tsx
@@ -0,0 +1,21 @@
+import { User } from "lucide-react-native";
+import { styled } from "nativewind";
+import { TextInput, TextInputProps, View } from "react-native";
+
+const InputStyled = ({ ...rest }: TextInputProps) => {
+  return (
+    <View className="relative flex max-w-[90%] items-center justify-center leading-7">
+      <View className="absolute left-4 h-5 w-5 fill-none">
+        <User size={20} strokeWidth={3} color={"#042A43"} />
+      </View>
+
+      <TextInput
+        className="mb-2 h-16 w-80 rounded-xl border-[1px] border-primary py-0 pl-12 text-lg text-primary placeholder-opacity-50 outline-none transition-all"
+        placeholderTextColor={"#012d4887"}
+        {...rest}
+      />
+    </View>
+  );
+};
+
+export const InputIcon = styled(InputStyled);
