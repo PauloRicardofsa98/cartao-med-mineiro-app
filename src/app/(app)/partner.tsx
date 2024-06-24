6f74cd335c4a70a751f7f5bc2e4c7391b20f8c2d
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-24 18:43:02 -0300

    feat: add PartnerScreen component for displaying partners

---
+++ src/app/(app)/partner.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PartnerScreen() {
+  return (
+    <View className="flex flex-1 items-center justify-center bg-red-500">
+      <Text>Parceiros</Text>
+    </View>
+  );
+}
