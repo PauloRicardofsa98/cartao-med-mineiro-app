6a407f5b275f14d595e9f58fde33467e325dd308
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-24 18:55:13 -0300

    refactor: move drawer-content.tsx to drawer folder

--- src/components/drawer/drawer-content.tsx
+++ src/components/drawer/drawer-content.tsx
@@ -4,24 +4,13 @@ import {
   DrawerItemList,
 } from "@react-navigation/drawer";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet } from "react-native";
 
 export const DrawerContent = (drawerProps: DrawerContentComponentProps) => {
   return (
     <DrawerContentScrollView {...drawerProps}>
-      <LinearGradient
-        colors={["#4c669f", "#3b5998", "#192f6a"]}
-        style={styles.background}
-      >
+      <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]}>
         <DrawerItemList {...drawerProps} />
       </LinearGradient>
     </DrawerContentScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    paddingTop: 20,
-  },
-});
