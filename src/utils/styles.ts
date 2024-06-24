7b46b6786c1d9cf6b801a95e60a115b37bf99d89
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-24 20:46:59 -0300

    refactor: add styles for shadow effects in utils/styles.ts

---
+++ src/utils/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+    elevation: 19,
+  },
+  shadow_sm: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+});
