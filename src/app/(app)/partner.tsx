c8a8ef29fda81aa76a6bf1c12dec16152e7c5df2
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-24 21:25:26 -0300

    refactor: optimize PartnerScreen component

--- src/app/(app)/partner.tsx
+++ src/app/(app)/partner.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, ScrollView } from "react-native";
+import { View, Text, Pressable, ScrollView, Image } from "react-native";
 
 import { ChevronDown, ChevronUp } from "lucide-react-native";
 import { useState } from "react";
@@ -11,7 +11,7 @@ export default function PartnerScreen() {
   const toggleOpen = () => setOpen(!open);
 
   return (
-    <View className="flex flex-1 items-center pt-4">
+    <View className="flex flex-1 items-center justify-center pt-4">
       <Pressable
         className="flex h-10 w-5/6 flex-row items-center justify-between rounded-xl bg-white px-4"
         style={styles.shadow}
@@ -35,7 +35,6 @@ export default function PartnerScreen() {
               <View key={index} className="h-10 w-[90%]" style={styles.shadow}>
                 <Button
                   variant="outline"
-                  shadow={true}
                   className="h-10 w-full items-start rounded-lg px-4 font-normal"
                 >
                   Formosa - Goiás
@@ -45,42 +44,42 @@ export default function PartnerScreen() {
           </ScrollView>
         </View>
       )}
-      <View className="flex h-32 w-full items-center justify-center bg-lime-500">
-        <Text>Teste2</Text>
-      </View>
 
-      {/* <DropDown open={open} setOpen={setOpen}>
-        <DropDownContent>
-          <DropDownLabel labelTitle="My Account" />
-          <DropDownItemSeparator />
-          <DropDownItem>
-            <TouchableOpacity className="flex flex-row items-center gap-2">
-              <CircleUser size={18} color="#fff" />
-              <Text className="text-xl text-primary">Profile</Text>
-            </TouchableOpacity>
-          </DropDownItem>
-          <DropDownItem>
-            <TouchableOpacity className="flex flex-row items-center gap-2">
-              <Settings size={18} color="#fff" />
-              <Text className="text-xl text-primary">Settings</Text>
-            </TouchableOpacity>
-          </DropDownItem>
-          <DropDownItem>
-            <TouchableOpacity className="flex flex-row items-center gap-2">
-              <CreditCard size={18} color="#fff" />
-              <Text className="text-xl text-primary">Billing</Text>
-            </TouchableOpacity>
-          </DropDownItem>
-          <DropDownLabel labelTitle="Team" />
-          <DropDownItemSeparator />
-          <DropDownItem>
-            <TouchableOpacity className="flex flex-row items-center gap-2">
-              <CreditCard size={18} color="#fff" />
-              <Text className="text-xl text-primary">Billing</Text>
-            </TouchableOpacity>
-          </DropDownItem>
-        </DropDownContent>
-      </DropDown> */}
+      <ScrollView
+        className="mt-4 w-full p-4"
+        contentContainerStyle={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 22,
+        }}
+      >
+        {Array.from({ length: 10 }).map((_, index) => (
+          <View
+            key={index}
+            className="w-full items-center justify-center gap-2 rounded-xl bg-white py-4"
+            style={styles.shadow}
+          >
+            <View className="h-64 w-[95%] rounded-xl">
+              <Image
+                source={require("../../assets/images/partner.png")}
+                className="h-full w-full rounded-xl"
+                resizeMode="contain"
+              />
+            </View>
+
+            <Text className="text-2xl font-bold text-black">
+              nome do parceiro
+            </Text>
+            <Text className="text-xl text-black">Beneficio do parceiro</Text>
+            <View className="flex flex-row items-center justify-center gap-1 px-2">
+              <Button className="h-11 flex-1">Contato</Button>
+              <Button className="h-11 flex-1">Endereço</Button>
+              <Button className="h-11 flex-1">Localização</Button>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
