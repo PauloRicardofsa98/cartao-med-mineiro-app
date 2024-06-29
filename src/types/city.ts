c5e337cc730ae45ccdf08c64c52cc8d21cea6322
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-28 22:22:01 -0300

    feat: add City and Partner types for data modeling

---
+++ src/types/city.ts
@@ -0,0 +1,8 @@
+export type City = {
+  uuid: string;
+  id: number;
+  name: string;
+  state: string;
+  created_at: Date;
+  updated_at: Date;
+};
---
+++ src/types/partner.ts
@@ -0,0 +1,12 @@
+export type Partner = {
+  uuid: string;
+  id: number;
+  name: string;
+  phone: string;
+  address: string;
+  banner: string;
+  benefit: string;
+  created_at: Date;
+  updated_at: Date;
+  cityUuid: string;
+};
