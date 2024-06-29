c3cb592c77f22a2ed9dfa4abf5ea46c9b1f26a81
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-06-28 22:22:12 -0300

    feat: add loading indicator and city selection functionality to PartnerScreen

--- src/app/(app)/partner.tsx
+++ src/app/(app)/partner.tsx
@@ -1,17 +1,83 @@
-import { View, Text, Pressable, ScrollView, Image } from "react-native";
-
+import {
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
 import { ChevronDown, ChevronUp } from "lucide-react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { styles } from "@/utils/styles";
+import { Partner } from "@/types/partner";
+import { api } from "@/services/api";
+import { City } from "@/types/city";
 
 export default function PartnerScreen() {
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [partners, setPartners] = useState<Partner[]>([]);
+  const [cities, setCities] = useState<City[]>([]);
+  const [selectedCity, setSelectedCity] = useState<City | undefined>();
+
+  useEffect(() => {
+    setLoading(true);
+    async function getCities() {
+      try {
+        const response = await api.get("/city");
+        const cities = response.data.data;
+        setCities(cities);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getCities();
+    setLoading(false);
+  }, []);
+
+  const handleSelectedCity = async (city: City) => {
+    setLoading(true);
+    setOpen(false);
+    setSelectedCity(city);
+    await getPartners();
+    setLoading(false);
+  };
+
+  async function getPartners() {
+    try {
+      const params = new URLSearchParams();
+
+      params.append(
+        "filters",
+        JSON.stringify({ cityUuid: selectedCity?.uuid }),
+      );
+
+      const response = await api.get(`/partner`, { params });
+      const partners = response.data.data;
+      setPartners(partners);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   const toggleOpen = () => setOpen(!open);
 
+  if (loading)
+    return (
+      <View className="flex flex-1 items-center justify-center">
+        <ActivityIndicator size={"large"} color={"#042A43"} />
+      </View>
+    );
+
   return (
-    <View className="flex flex-1 items-center justify-center pt-4">
+    <Animatable.View
+      className={`flex flex-1 items-center ${!selectedCity ? "justify-center" : "justify-start"} pt-4`}
+      animation={"pulse"}
+      duration={1000}
+      iterationCount={5}
+    >
       <Pressable
         className="flex h-10 w-5/6 flex-row items-center justify-between rounded-xl bg-white px-4"
         style={styles.shadow}
@@ -26,60 +92,71 @@ export default function PartnerScreen() {
       </Pressable>
 
       {open && (
-        <View className="relative z-50 flex h-auto w-[90%] items-center justify-center">
+        <View className="relative z-50 flex h-[85%] w-[90%] items-center justify-center">
           <ScrollView
-            className="absolute top-5 z-50 flex w-full min-w-[8rem] gap-3 rounded-xl bg-white p-3"
+            className="absolute top-5 z-50 flex h-full w-full min-w-[8rem] gap-3 rounded-xl bg-white py-3"
+            contentContainerStyle={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 10,
+            }}
             style={styles.shadow_sm}
           >
-            {Array.from({ length: 10 }).map((_, index) => (
+            {cities.map((city, index) => (
               <View key={index} className="h-10 w-[90%]" style={styles.shadow}>
                 <Button
                   variant="outline"
                   className="h-10 w-full items-start rounded-lg px-4 font-normal"
+                  onPress={() => handleSelectedCity(city)}
                 >
-                  Formosa - Goiás
+                  {city.name} - {city.state}
                 </Button>
               </View>
             ))}
           </ScrollView>
         </View>
       )}
+      {!open && selectedCity && (
+        <ScrollView
+          className="w-ful mt-4 p-4"
+          contentContainerStyle={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 22,
+          }}
+        >
+          {partners.map((partner, index) => (
+            <View
+              key={index}
+              className="w-full items-center justify-center gap-2 rounded-xl bg-white py-4"
+              style={styles.shadow}
+            >
+              <View className="h-64 w-[95%] rounded-xl">
+                <Image
+                  source={{
+                    uri: `https://apiv2.cartaomedmineiro.com.br/upload/${partner.banner}`,
+                    // uri: `http://192.168.18.95:8000/upload/${partner.banner}`,
+                  }}
+                  className="h-full w-full rounded-xl"
+                  resizeMode="contain"
+                />
+              </View>
 
-      <ScrollView
-        className="mt-4 w-full p-4"
-        contentContainerStyle={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 22,
-        }}
-      >
-        {Array.from({ length: 10 }).map((_, index) => (
-          <View
-            key={index}
-            className="w-full items-center justify-center gap-2 rounded-xl bg-white py-4"
-            style={styles.shadow}
-          >
-            <View className="h-64 w-[95%] rounded-xl">
-              <Image
-                source={require("../../assets/images/partner.png")}
-                className="h-full w-full rounded-xl"
-                resizeMode="contain"
-              />
+              <Text className="text-2xl font-bold text-black">
+                {partner.name}
+              </Text>
+              <Text className="text-xl text-black">{partner.benefit}</Text>
+              <View className="flex flex-row items-center justify-center gap-1 px-2">
+                <Button className="h-11 flex-1">Contato</Button>
+                <Button className="h-11 flex-1">Endereço</Button>
+                <Button className="h-11 flex-1">Localização</Button>
+              </View>
             </View>
-
-            <Text className="text-2xl font-bold text-black">
-              nome do parceiro
-            </Text>
-            <Text className="text-xl text-black">Beneficio do parceiro</Text>
-            <View className="flex flex-row items-center justify-center gap-1 px-2">
-              <Button className="h-11 flex-1">Contato</Button>
-              <Button className="h-11 flex-1">Endereço</Button>
-              <Button className="h-11 flex-1">Localização</Button>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+          ))}
+        </ScrollView>
+      )}
+    </Animatable.View>
   );
 }
